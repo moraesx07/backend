25f36e818577ffa240114dc3e33905a0cf299b97
guilherme <159555227+moraesx07@users.noreply.github.com>
2025-03-11 11:57:51 +0000

    Payment Controller and Router

---
+++ src/controllers/payment.controller.js
@@ -0,0 +1,12 @@
+const PaymentController = {
+    async createPayment(req,res){
+        try {
+            const {data, recibo, valor, observacao} = req.body;
+            return res.status(201).json({message: 'Payment created', data: {data, recibo, valor, observacao}}); 
+        } catch (error) {
+            return res.status(500).json({message: 'Internal server error'});
+        }
+    }
+};
+
+export default PaymentController;
