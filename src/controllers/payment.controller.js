bc68e9bdee8f0f2b02a4599aeb2dd5b15e4b4f5f
guilherme <159555227+moraesx07@users.noreply.github.com>
2025-03-18 10:52:23 +0000

    ultima aula

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,9 +1,22 @@
+import { z } from "zod";
+
+const PaymentSchema = z.object({
+    data: z.string().datetime({message: 'Data inválida'}),
+    recibo: z.number({message: "recibo invalido"}).min(1, {message: 'Recibo inválido'}),
+    valor: z.number({message: "valor invalido"}).min(0, {message: 'Valor inválido'}),
+    observacao: z.string().optional(),
+});
+
 const PaymentController = {
     async createPayment(req,res){
         try {
             const {data, recibo, valor, observacao} = req.body;
+            PaymentSchema.parse({data, recibo, valor, observacao});
             return res.status(201).json({message: 'Payment created', data: {data, recibo, valor, observacao}}); 
         } catch (error) {
+            if (error instanceof z.ZodError) {
+                return res.status(400).json({message: "validation error", details: error.errors});
+            }
             return res.status(500).json({message: 'Internal server error'});
         }
     }
